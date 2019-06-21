9ffae76551110524bf910e830f5bd7cd39db0857
RadTrap <44319561+Radthiek@users.noreply.github.com>
2019-06-21 15:13:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ fs.readdir("./commands/", (err, files) => {
 
 });
 
-// //Setting up the bots presences
+
 bot.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
+
   console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
